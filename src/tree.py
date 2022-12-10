e521c34c18cc6c066c01c772c714a4dbc77385fa
KlogeBukser <imvacant0923@gmail.com>
2022-12-10 18:19:30 +0100

    update

--- src/tree.py
+++ src/tree.py
@@ -2,10 +2,13 @@
 import numpy as np
 import sys
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 from read_data import *
 from plot_tools import *
 # from NN.misc import accuracy_score
 
+def 
 max_depth = int(sys.argv[1])
 
 x_train, x_test, y_train, y_test = read_wine_data()
@@ -14,3 +17,5 @@ tree_clf = DecisionTreeClassifier(max_depth=max_depth, random_state=42)
 tree_clf.fit(x_train, y_train)
 y_pred = tree_clf.predict(x_test)
 print(tree_clf.score(x_test, y_test))
+
+
